a7895d2ec25455356d818a51c4230fe1efa89cd3
Renan <machadosantosrenan@gmail.com>
2025-11-10 22:02:56 -0300

    Template Loaded

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-redLight font-family-poppins">
+      <p className="text-white">Boa sorte no projeto! <br /> - Menoni ❤️</p>
+    </div>
+  )
+}
+
+export default App
